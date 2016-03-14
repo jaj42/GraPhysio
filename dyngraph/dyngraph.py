c600c276cb5818e32beb20620b9e272a111997de
Jona Joachim <jaj@joachim.cc>
2016-03-14 22:19:20 +0100

    Add date parsing code. Add a PlotInfo class which retains info about the plot. Change ListWidgets to ListViews to add checkboxes for date parsing inside.

--- dyngraph/dyngraph.py
+++ dyngraph/dyngraph.py
@@ -25,23 +25,26 @@ class MainUi(QtGui.QMainWindow, Ui_MainWindow):
         self.menuFile.addSeparator()
         self.menuFile.addAction('&Quit', self.fileQuit, Qt.CTRL + Qt.Key_Q)
 
+
     def requestNewPlot(self):
         dlgNewplot = DlgNewPlot()
         if not dlgNewplot.exec_(): return
 
         workerthread = QtCore.QThread()
-        csvreader = dlgNewplot.result
+        plotinfo = dlgNewplot.result
+        csvreader = Reader(plotinfo)
         self.__workers.append((workerthread, csvreader))
-
+        self.statusBar.showMessage("Loading... {}...".format(plotinfo.plotname))
         csvreader.sigData.connect(self.createNewPlotWithData)
         csvreader.moveToThread(workerthread)
         workerthread.started.connect(csvreader.start)
         workerthread.start()
 
-    def createNewPlotWithData(self, data):
-        plot = plotwidget.PlotWidget(parent=self, plotdata=data)
-        tabindex = self.tabWidget.addTab(plot, "test")
+    def createNewPlotWithData(self, plotinfo):
+        plot = plotwidget.PlotWidget(parent=self, plotinfo=plotinfo)
+        tabindex = self.tabWidget.addTab(plot, plotinfo.plotname)
         self.tabWidget.setCurrentIndex(tabindex)
+        self.statusBar.showMessage("Loading... done".format(plotinfo.plotname))
 
     def fileQuit(self):
         self.close()
@@ -51,33 +54,29 @@ class MainUi(QtGui.QMainWindow, Ui_MainWindow):
         self.tabWidget.removeTab(i)
         w.destroy()
 
-    @property
-    def statusmessage(self):
-        return self.statusBar.currentMessage()
-
-    @statusmessage.setter
-    def statusmessage(self, m):
-        if m is None:
-            self.statusBar.clearMessage()
-        else:
-            self.statusBar.showMessage(m)
-
 
 class Reader(QtCore.QObject):
     sigData = QtCore.pyqtSignal(object)
 
-    def __init__(self, filename, seperator, fields):
+    def __init__(self, plotinfo):
         super(Reader, self).__init__()
-        self.filename  = filename
-        self.seperator = seperator
-        self.fields    = fields
+        self.plotinfo = plotinfo
 
     def start(self):
-        data = pandas.read_csv(self.filename,
-                               sep     = self.seperator,
-                               usecols = self.fields,
+        if self.plotinfo.xisdate:
+            datefield = self.plotinfo.xfields
+        else:
+            datefield = False
+        data = pandas.read_csv(self.plotinfo.filename,
+                               sep     = self.plotinfo.seperator,
+                               usecols = self.plotinfo.fields,
+                               decimal = self.plotinfo.decimal,
+                               parse_dates = datefield,
+                               date_parser = self.plotinfo.datetime_parser,
+                               index_col = False,
                                engine  = 'c')
-        self.sigData.emit(data)
+        self.plotinfo.plotdata = data
+        self.sigData.emit(self.plotinfo)
 
 
 class DlgNewPlot(QtGui.QDialog, Ui_NewPlot):
@@ -85,34 +84,31 @@ class DlgNewPlot(QtGui.QDialog, Ui_NewPlot):
         super(DlgNewPlot, self).__init__(parent=parent)
         self.setupUi(self)
 
-        self.xfields = []
-        self.yfields = []
-        self.filename = ""
-        self.sep = ""
+        self.plotinfo = plotwidget.PlotInfo()
 
-        self.txtSep.addItem(',')
-        self.txtSep.addItem(';')
-
-        self.txtEvery.setMinimum(1)
-        self.txtEvery.setMaximum(1000)
+        # Attach models to ListViews
+        self.lstX = QtGui.QStandardItemModel()
+        self.lstY = QtGui.QStandardItemModel()
+        self.lstAll = QtGui.QStandardItemModel()
+        self.lstVX.setModel(self.lstX)
+        self.lstVY.setModel(self.lstY)
+        self.lstVAll.setModel(self.lstAll)
 
         # Connect callbacks
         self.btnBrowse.clicked.connect(self.selectFile)
         self.btnLoad.clicked.connect(self.loadCsvFields)
         self.btnOk.clicked.connect(self.loadPlot)
         self.btnCancel.clicked.connect(self.reject)
-
-        # Hack since you cannot pass arguments to Qt's signal connect()
-        #def moveToX():   return self.moveFromTo(self.listAll, self.listX)
-        def moveToY():   return self.moveFromTo(self.listAll, self.listY)
-        def moveFromX(): return self.moveFromTo(self.listX,   self.listAll)
-        def moveFromY(): return self.moveFromTo(self.listY,   self.listAll)
         self.btnToX.clicked.connect(self.moveToX)
-        self.btnToY.clicked.connect(moveToY)
-        self.btnRemoveX.clicked.connect(moveFromX)
-        self.btnRemoveY.clicked.connect(moveFromY)
+        self.btnToY.clicked.connect(self.moveToY)
+        self.btnRemoveX.clicked.connect(self.delFromX)
+        self.btnRemoveY.clicked.connect(self.delFromY)
+        self.chkUnixTime.stateChanged.connect(self.boolUnixtime)
 
-    # Methods
+    # Methods / Callbacks
+    def boolUnixtime(self, state):
+        self.txtDateTime.setEnabled(not self.chkUnixTime.isChecked())
+
     def selectFile(self):
         self.txtFile.setText(QtGui.QFileDialog.getOpenFileName())
 
@@ -125,38 +121,69 @@ class DlgNewPlot(QtGui.QDialog, Ui_NewPlot):
             for row in csv.DictReader(csvfile, delimiter=sep):
                 fields = row.keys()
                 break
-        # Clear all lists, then add new fields
-        map(lambda lst: lst.clear(), [self.listAll, self.listX, self.listY])
+        # Clear all lsts, then add new fields
+        map(lambda lst: lst.clear(), [self.lstAll, self.lstX, self.lstY])
         for field in fields:
-            self.listAll.addItem(field)
-
-    def moveFromTo(self, fromList, toList):
-        while True:
-            rowindexes = fromList.selectedIndexes()
-            if len(rowindexes) < 1: break
-            row = rowindexes[0].row()
-            toList.addItem(fromList.takeItem(row))
+            if field is None: continue
+            item = QtGui.QStandardItem(field)
+            self.lstAll.appendRow(item)
 
     def moveToX(self):
         # Only allow one element in X for now.
-        if self.listX.count() > 0: return
-        rows = [index.row() for index in self.listAll.selectedIndexes()]
+        if self.lstX.rowCount() > 0: return
+        rows = [index.row() for index in self.lstVAll.selectedIndexes()]
         for row in rows:
-            self.listX.addItem(self.listAll.takeItem(row))
+            rowItems = self.lstAll.takeRow(row)
+            item = rowItems[0]
+            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
+            item.setData(QtCore.QVariant(Qt.Unchecked), Qt.CheckStateRole)
+            self.lstX.appendRow(item)
             break
 
+    def moveToY(self):
+        while True:
+            rowindexes = self.lstVAll.selectedIndexes()
+            if len(rowindexes) < 1: break
+            row = rowindexes[0].row()
+            self.lstY.appendRow(self.lstAll.takeRow(row))
+
+    def delFromX(self):
+        while True:
+            rowindexes = self.lstVX.selectedIndexes()
+            if len(rowindexes) < 1: break
+            row = rowindexes[0].row()
+            rowItems = self.lstX.takeRow(row)
+            item = rowItems[0]
+            item.setFlags(item.flags() ^ QtCore.Qt.ItemIsUserCheckable)
+            item.setData(QtCore.QVariant(), Qt.CheckStateRole)
+            self.lstAll.appendRow(item)
+
+    def delFromY(self):
+        while True:
+            rowindexes = self.lstVY.selectedIndexes()
+            if len(rowindexes) < 1: break
+            row = rowindexes[0].row()
+            self.lstAll.appendRow(self.lstY.takeRow(row))
+
     def loadPlot(self):
-        xRows = [i.text() for i in self.listX.findItems("", Qt.MatchContains)]
-        yRows = [i.text() for i in self.listY.findItems("", Qt.MatchContains)]
-        self.xfields = map(str, xRows)
-        self.yfields = map(str, yRows)
-        self.filename = str(self.txtFile.text())
-        self.sep = str(self.txtSep.currentText())
+        yRows = [i.text() for i in self.lstY.findItems("", Qt.MatchContains)]
+        xRows = [i.text() for i in self.lstX.findItems("", Qt.MatchContains)]
+        xState = [i.checkState() for i in self.lstX.findItems("", Qt.MatchContains)]
+        for s in xState:
+            self.plotinfo.xisdate = s > Qt.Unchecked
+            break
+        self.plotinfo.xfields = set(map(str, xRows))
+        self.plotinfo.yfields = set(map(str, yRows))
+        self.plotinfo.filename = str(self.txtFile.text())
+        self.plotinfo.seperator = str(self.txtSep.currentText())
+        self.plotinfo.decimal = str(self.txtDecimal.currentText())
+        self.plotinfo.datetime_format = str(self.txtDateTime.currentText())
+        self.plotinfo.isunixtime = self.chkUnixTime.isChecked()
         self.accept()
 
     @property
     def result(self):
-        return Reader(self.filename, self.sep, self.xfields + self.yfields)
+        return self.plotinfo
 
 
 if __name__ == '__main__':
--- dyngraph/plotwidget.py
+++ dyngraph/plotwidget.py
@@ -1,16 +1,20 @@
+import os
+from datetime import datetime
+
 from PyQt4 import QtCore, QtGui
 import pyqtgraph as pg
 import numpy as np
 
 class PlotWidget(pg.PlotWidget):
-    def __init__(self, plotdata, parent=None):
+    def __init__(self, plotinfo, parent=None):
         super(PlotWidget, self).__init__(parent=parent)
         vb = self.getViewBox()
         vb.setMouseMode(vb.RectMode)
+        #vb.menu = None
 
         self.addLegend()
 
-        for n, column in enumerate(plotdata.iteritems()):
+        for n, column in enumerate(plotinfo.plotdata.iteritems()):
             colname, series = column
             curve = DynPlot(series, n, name=colname)
             self.addItem(curve)
@@ -25,8 +29,42 @@ class DynPlot(pg.PlotCurveItem):
             color = 'w'
         else:
             color = self.colors[plotnumber]
-
+    
         super(DynPlot, self).__init__(x   = np.array(plotdata.index),
                                       y   = plotdata.values,
                                       pen = color,
                                       *args, **kwds)
+
+class PlotInfo():
+    def __init__(self, filename  = "",
+                       seperator = ",",
+                       decimal   = ".",
+                       xfields = [],
+                       yfields = [],
+                       xisdate = False,
+                       isunixtime = False,
+                       datetime_format = "%Y-%m-%d %H:%M:%S,%f"):
+        self.filename = filename
+        self.seperator = seperator
+        self.decimal = decimal
+        self.xfields = set(xfields)
+        self.yfields = set(yfields)
+        self.xisdate = xisdate
+        self.datetime_format = datetime_format
+        self.isunixtime = isunixtime
+        self.plotdata = None
+
+    @property
+    def datetime_parser(self):
+        def conv_datetime(datestr):
+            return datetime.strptime(datestr, self.datetime_format)
+        return conv_datetime
+
+    @property
+    def fields(self):
+        return self.xfields | self.yfields
+
+    @property
+    def plotname(self):
+        plotname, ext = os.path.splitext(os.path.basename(self.filename))
+        return plotname
