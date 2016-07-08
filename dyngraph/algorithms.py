b5852081a7cf587f45553027194be1ed4ff06a22
jaj <jona@joachim.cc>
2016-07-08 15:04:59 +0200

    Implement the new adaptive algorithm for pulse detection.

--- dyngraph/algorithms.py
+++ dyngraph/algorithms.py
@@ -3,20 +3,42 @@ import sys
 import numpy as np
 import pandas as pd
 
-def findPressureFeet(series):
-    sndderiv = series.diff().diff().shift(-2)
-    try:
-        threshold = np.percentile(sndderiv.dropna(), 98)
-    except IndexError as e:
-        print("percentile error: {}".format(e), file=sys.stderr)
-        return pd.Series()
+def calcSampleRate(series):
+    if type(series.index) != pd.tseries.index.DatetimeIndex:
+        print("No time information for cycle identification", file=sys.stderr)
+        return 125
+    starttime = series.index.values[0]
+    stopidx = series.index.get_loc(starttime + pd.Timedelta('1s'), method='nearest')
+    return stopidx
 
-    peaks = np.diff((sndderiv > threshold).astype(int))
-    peakStarts = np.flatnonzero(peaks > 0)
-    peakStops  = np.flatnonzero(peaks < 0)
+def pulsePeaks(series, deriv):
+    samplerate = calcSampleRate(series)
+    winsum   = int(samplerate / 4)
+    winquant = int(samplerate * 3)
+    derivsq  = deriv ** 2
+    integral = derivsq.rolling(window=winsum,center=True).sum()
+    thres    = integral.rolling(window=winquant).quantile(.7)
+    thres    = thres.fillna(method='backfill')
+    return (integral > thres).astype(int)
+
+def findPressureFeet(series):
+    fstderiv = series.diff().shift(-1)
+    sndderiv = fstderiv.diff().shift(-1)
+
+    # Remove deceleration peaks
+    sndderiv = sndderiv * (fstderiv > 0)
+
+    peaks = pulsePeaks(series, sndderiv)
+    peakvar = np.diff(peaks.astype(int))
+    peakStarts = np.flatnonzero(peakvar > 0)
+    peakStops  = np.flatnonzero(peakvar < 0)
 
     # Handle the case where we start near a foot
-    peakStops = peakStops[peakStops > peakStarts[0]]
+    try:
+        peakStops = peakStops[peakStops > peakStarts[0]]
+    except IndexError as e:
+        print("No foot detected: {}".format(e), file=sys.stderr)
+        return pd.Series()
 
     def locateMaxima():
         for start, stop in zip(peakStarts, peakStops):
