023ad2f1420f151db934813a798eeb1d7a194371
jaj <jona@joachim.cc>
2016-12-03 18:10:20 +0100

    Support cxfreeze

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
-from setuptools import setup
+import sys
+from cx_Freeze import setup, Executable
 
-setup(name='dyngraph',
-      version='0.2',
-      description='Dynamic graph view',
-      url='https://git.joachim.cc/cgit.cgi/dyngraph/',
-      author='Jona JOACHIM',
-      author_email='jona@joachim.cc',
-      license='ISC',
-      packages=['dyngraph'],
-      install_requires=[
-          'pyqtgraph',
-          'pandas'
-      ],
-      scripts = [
-          'scripts/dyngraph.pyw'
-      ]
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(name = 'dyngraph',
+      version = '0.3',
+      description = 'Dynamic graph view',
+      url = 'https://git.joachim.cc/cgit.cgi/dyngraph/',
+      author = 'Jona JOACHIM',
+      author_email = 'jona@joachim.cc',
+      license = 'ISC',
+      install_requires = ['pyqtgraph', 'pandas'],
+      scripts = ['scripts/dyngraph.pyw'],
+      packages = ['dyngraph', 'dyngraph.ui'],
+      options = {"build_exe" : build_exe_options},
+      executables = [Executable("scripts/dyngraph.pyw", base=base)]
 )
