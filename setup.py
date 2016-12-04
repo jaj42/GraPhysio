b240f0f57b707a15e23c535ff5750e159ed23e2e
jaj <jona@joachim.cc>
2016-12-04 21:18:57 +0100

    cxfreeze does not work. remove

--- setup.py
+++ setup.py
@@ -1,25 +1,13 @@
-import sys
-from cx_Freeze import setup, Executable
-
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
-
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"
+from setuptools import setup
 
 setup(name = 'dyngraph',
       version = '0.3',
       description = 'Dynamic graph view',
-      url = 'https://git.joachim.cc/cgit.cgi/dyngraph/',
+      url = 'https://github.com/jaj42/dyngraph',
       author = 'Jona JOACHIM',
       author_email = 'jona@joachim.cc',
       license = 'ISC',
       install_requires = ['pyqtgraph', 'pandas'],
       scripts = ['scripts/dyngraph.py'],
       packages = ['dyngraph', 'dyngraph.ui'],
-      options = {"build_exe" : build_exe_options},
-      executables = [Executable("scripts/dyngraph.py", base=base)]
 )
