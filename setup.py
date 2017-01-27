1cf88ff92ae4c0cf577f0b80c5484a4df3262bee
jaj <jona@joachim.cc>
2017-01-27 11:33:21 +0100

    Add modified script name to setup.py...

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name = 'graphysio',
-      version = '0.5',
+      version = '0.6',
       description = 'Graphical visualization of physiologic time series',
       url = 'https://github.com/jaj42/graphysio',
       author = 'Jona JOACHIM',
       author_email = 'jona@joachim.cc',
       license = 'ISC',
       install_requires = ['pyqtgraph', 'pandas'],
-      scripts = ['scripts/graphysio.py'],
+      scripts = ['scripts/graphysioui.py'],
       packages = ['graphysio', 'graphysio.ui'],
 )
