8420bf735b64ceaba965b946c1f11c77b0732038
jaj <jona@joachim.cc>
2017-01-27 11:48:37 +0100

    Add python 3 dependency, bump minor

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name = 'graphysio',
-      version = '0.6',
+      version = '0.61',
       description = 'Graphical visualization of physiologic time series',
       url = 'https://github.com/jaj42/graphysio',
       author = 'Jona JOACHIM',
       author_email = 'jona@joachim.cc',
       license = 'ISC',
+      python_requires = '>=3.4',
       install_requires = ['pyqtgraph', 'pandas'],
       scripts = ['scripts/graphysioui.py'],
       packages = ['graphysio', 'graphysio.ui'],
