95f2f1f7485e274fa99438451185ca7b06078b39
jaj <jona@joachim.cc>
2017-03-28 10:17:24 +0200

    Bump minor

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'graphysio',
-      version = '0.83',
+      version = '0.84',
       description = 'Graphical visualization of physiologic time series',
       url = 'https://github.com/jaj42/graphysio',
       author = 'Jona JOACHIM',
