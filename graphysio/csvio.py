0d26149e73df65c432399aed8a1d73fb2d6a64dc
Jona Joachim <jona@joachim.cc>
2019-03-28 12:27:01 +0100

    Make sure the timestamp for data is unique. When duplicated timestamps are detected, use the mean value for the data

--- graphysio/csvio.py
+++ graphysio/csvio.py
@@ -37,33 +37,44 @@ class Reader(QtCore.QRunnable):
         dtformat = self.csvrequest.datetime_format
         if self.csvrequest.generatex:
             data.index = (1e9 * data.index / self.csvrequest.samplerate).astype(np.int64)
-        elif dtformat == '<infer>':
-            timestamp = data[self.csvrequest.dtfield].dropna()
-            timestamp = pd.to_datetime(timestamp, infer_datetime_format=True)
+            # Make all data numeric and remove empty rows
+            data = data.apply(pdtonum)
+            data = data.dropna(axis='rows', how='all')
         else:
+            datacolumns = data.columns.drop(self.csvrequest.dtfield)
+            # Make all data numeric or drop if impossible
+            data[datacolumns] = data[datacolumns].apply(pdtonum)
+            # Make timestamp unique and use mean of values on duplicates
+            data = data.groupby(self.csvrequest.dtfield, as_index=False).mean()
+            # Remove empty rows
+            data = data.dropna(axis='rows', how='all', subset=datacolumns)
+            # Remove timestamp column from data
             timestamp = data[self.csvrequest.dtfield]
+            data = data.drop(columns=self.csvrequest.dtfield)
+
             if dtformat == '<seconds>':
-                timestamp = pdtonum(timestamp).dropna()
+                timestamp = pdtonum(timestamp)
                 timestamp = pd.to_datetime(timestamp * 1e9, unit = 'ns')
             elif dtformat == '<milliseconds>':
-                timestamp = pdtonum(timestamp).dropna()
+                timestamp = pdtonum(timestamp)
                 timestamp = pd.to_datetime(timestamp * 1e6, unit = 'ns')
             elif dtformat == '<microseconds>':
-                timestamp = pdtonum(timestamp).dropna()
+                timestamp = pdtonum(timestamp)
                 timestamp = pd.to_datetime(timestamp * 1e3, unit = 'ns')
             elif dtformat == '<nanoseconds>':
-                timestamp = pdtonum(timestamp).dropna()
+                timestamp = pdtonum(timestamp)
                 timestamp = pd.to_datetime(timestamp, unit = 'ns')
+            elif dtformat == '<infer>':
+                timestamp = pd.to_datetime(timestamp, infer_datetime_format=True)
             else:
                 timestamp = pd.to_datetime(timestamp, format = dtformat)
+
+            # Convert timestamp to UTC
             timestamp = pd.Index(timestamp).tz_localize(self.csvrequest.timezone).tz_convert('UTC')
             timestamp = timestamp.astype(np.int64)
             data = data.set_index([timestamp])
-            data = data.drop(columns=self.csvrequest.dtfield)
 
-        # Coerce all columns to numeric and remove empty columns
-        data = data.apply(pdtonum).dropna(axis='columns', how='all')
-        data = data.dropna(axis='rows', how='all')
+        data = data.dropna(axis='columns', how='all')
         data = data.sort_index()
 
         plotdata = PlotData(data = data, filepath = self.csvrequest.filepath)
