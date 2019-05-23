b0826d8324c2c0c71cf7f9ee1490510ac2987f6c
Jona Joachim <jona@joachim.cc>
2019-05-23 09:37:51 +0200

    On error, send log by mail for ease of debugging

--- scripts/graphysioui.py
+++ scripts/graphysioui.py
@@ -4,10 +4,24 @@ import sys
 from pyqtgraph.Qt import QtGui
 from graphysio.mainui import MainUi
 
+import traceback
+import logging, logging.handlers
+
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
 
     winmain = MainUi()
     winmain.show()
 
-    sys.exit(app.exec_())
+    log = logging.getLogger("GraPhysio")
+    smtph = logging.handlers.SMTPHandler('puff.joachim.cc', 'graphysio@example.com', 'jaj_graphysio@joachim.cc', 'GraPhysio_error')
+    log.addHandler(smtph)
+
+    status = 1
+    try:
+        status = app.exec_()
+    except:
+        tb = traceback.format_exc()
+        log.error(tb)
+    finally:
+        sys.exit(status)
