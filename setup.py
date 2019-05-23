058960bae3e36bf1ea83022e5139b2fd42606fd0
Jona Joachim <jona@joachim.cc>
2019-05-23 15:42:36 +0200

    Update dependencies. Bump minor

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name = 'graphysio',
-      version = '0.106',
+      version = '0.107',
       description = 'Graphical visualization of physiologic time series',
       url = 'https://github.com/jaj42/graphysio',
       author = 'Jona JOACHIM',
       author_email = 'jona@joachim.cc',
       license = 'ISC',
       python_requires = '>=3.4',
-      install_requires = ['pyqtgraph', 'pandas', 'numpy', 'scipy', 'sympy'],
+      install_requires = ['pyqtgraph', 'pandas', 'numpy', 'scipy', 'sympy', 'pathvalidate'],
       scripts = ['scripts/graphysioui.py'],
       packages = ['graphysio', 'graphysio.plotwidgets', 'graphysio.ui'],
       include_package_data = True
