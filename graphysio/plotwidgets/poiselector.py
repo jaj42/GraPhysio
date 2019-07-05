7681db273867b05686d579a325a048a507477b60
Jona Joachim <jona@joachim.cc>
2019-07-05 12:09:16 +0200

    Remove remnants of waveform.py from filters.py

--- graphysio/plotwidgets/poiselector.py
+++ graphysio/plotwidgets/poiselector.py
@@ -7,7 +7,8 @@ from PyQt5 import QtWidgets, QtGui
 
 from graphysio import exporter, ui
 from graphysio.plotwidgets import PlotWidget
-from graphysio.algorithms.filters import findPOIGreedy, savgol
+from graphysio.algorithms.filters import savgol
+from graphysio.algorithms.waveform import findPOIGreedy
 
 class FixIndex(Enum):
     disabled = 'Disabled'
