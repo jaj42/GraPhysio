02900eb4d6e7063fe0d7c23b45d89468207074a7
Jona Joachim <jona@joachim.cc>
2020-05-13 17:21:28 +0200

    Add EDF read support

--- graphysio/readdata/__init__.py
+++ graphysio/readdata/__init__.py
@@ -1,5 +1,6 @@
 from .csv import CsvReader, DlgNewPlotCsv
 from .parquet import ParquetReader, DlgNewPlotParquet
+from .edf import EdfReader, DlgNewPlotEdf
 
-readers = {'csv': CsvReader, 'parquet': ParquetReader}
-dlgNewPlots = {'csv': DlgNewPlotCsv, 'parquet': DlgNewPlotParquet}
+readers = {'csv': CsvReader, 'parquet': ParquetReader, 'edf': EdfReader}
+dlgNewPlots = {'csv': DlgNewPlotCsv, 'parquet': DlgNewPlotParquet, 'edf': DlgNewPlotEdf}
---
+++ graphysio/readdata/edf.py
@@ -0,0 +1,83 @@
+import os
+
+import pyedflib
+import numpy as np
+import pandas as pd
+
+from pyqtgraph.Qt import QtGui, QtCore
+
+from graphysio import types
+
+
+class EdfReader(QtCore.QRunnable):
+    def __init__(self, datarequest, sigdata, sigerror):
+        super().__init__()
+        self.filepath = datarequest.filepath
+        self.sigdata = sigdata
+        self.sigerror = sigerror
+
+    def run(self) -> None:
+        try:
+            data = self.getdata()
+        except Exception as e:
+            self.sigerror.emit(e)
+        else:
+            self.sigdata.emit(data)
+
+    def getdata(self) -> types.PlotData:
+        signals = []
+
+        edf = pyedflib.EdfReader(self.filepath)
+        beginns = edf.getStartdatetime().timestamp() * 1e9
+        nsamplesPerChannel = edf.getNSamples()
+
+        for i in range(edf.signals_in_file):
+            h = edf.getSignalHeader(i)
+            fs = h['sample_rate']
+            n = nsamplesPerChannel[i]
+            endns = beginns + n * 1e9 / fs
+            idx = np.linspace(beginns, endns, num=n, dtype=np.int64)
+            s = pd.Series(edf.readSignal(i), index=idx, name=h['label'])
+            signals.append(s)
+        edf.close()
+
+        df = pd.concat(signals, axis=1)
+        plotdata = types.PlotData(data=df, filepath=self.filepath)
+
+        return plotdata
+
+
+class DataRequest:
+    def __init__(self, filepath=""):
+        self.filepath = filepath
+
+    @property
+    def name(self):
+        name, _ = os.path.splitext(os.path.basename(self.filepath))
+        return name
+
+    @property
+    def folder(self):
+        folder = os.path.dirname(self.filepath)
+        return folder
+
+
+class DlgNewPlotEdf(QtCore.QObject):
+    dlgdata = QtCore.pyqtSignal(object)
+
+    def __init__(self, parent=None, title="New Plot", directory=""):
+        super().__init__(parent=parent)
+        self.datarequest = DataRequest()
+        self.dircache = directory
+
+    def exec_(self):
+        filepath = QtGui.QFileDialog.getOpenFileName(
+            parent=None, caption="Open Edf file", directory=self.dircache
+        )
+        # PyQt5 API change
+        if type(filepath) is not str:
+            filepath = filepath[0]
+
+        if filepath:
+            self.datarequest.filepath = filepath
+            self.dlgdata.emit(self.datarequest)
