07cb67ed77a63962e35c962103ccf18e058cd8b1
Jona Joachim <jona@joachim.cc>
2020-06-16 15:11:33 +0200

    Add Matlab .mat file export support

--- graphysio/writedata/__init__.py
+++ graphysio/writedata/__init__.py
@@ -1,9 +1,11 @@
 from .csv import curves_to_csv
 from .parquet import curves_to_parquet
 from .edf import curves_to_edf
+from .matlab import curves_to_matlab
 
 curve_writers = {
     'csv': curves_to_csv,
     'edf': curves_to_edf,
     'parquet': curves_to_parquet,
+    'mat': curves_to_matlab,
 }
---
+++ graphysio/writedata/matlab.py
@@ -0,0 +1,16 @@
+from typing import List
+
+import numpy as np
+import pandas as pd
+import scipy.io
+
+from graphysio.plotwidgets.curves import CurveItem
+
+
+def curves_to_matlab(
+    curves: List[CurveItem], filepath: str, index_label: str = 'timens'
+) -> None:
+    sers = [c.series for c in curves]
+    data = pd.concat(sers, axis=1).sort_index()
+    data['timens'] = data.index.astype(np.int64)
+    scipy.io.savemat(filepath, {filepath.stem: data.to_dict("list")})
