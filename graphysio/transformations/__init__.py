8ff732a60cbfb5213f9d55eecaf4c1f5738fdad4
Jona Joachim <jona@joachim.cc>
2022-09-13 14:49:54 +0200

    Update dependencies. Bump version.

--- graphysio/transformations/__init__.py
+++ graphysio/transformations/__init__.py
@@ -3,7 +3,7 @@ from .perfusion_index import get_perfusion_index
 from .precise_feet import get_precise_feet
 
 Transformations = {
-    'Perfusion Index': get_perfusion_index,
-    'Feet to Curve': get_feet_to_curve,
     'Precise Feet': get_precise_feet,
+    'Feet to Curve': get_feet_to_curve,
+    'Perfusion Index': get_perfusion_index,
 }
