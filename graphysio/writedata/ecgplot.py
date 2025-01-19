e1273c8957f1a1f2852bdc221f1d3abdbfdd3d73
Jona JOACHIM <jona@joachim.cc>
2025-01-19 18:40:41 +0100

    Add ecg plot

--- graphysio/writedata/ecgplot.py
+++ graphysio/writedata/ecgplot.py
@@ -1,10 +1,22 @@
 from typing import List
 
-import pandas as pd
-
 from graphysio.plotwidgets.curves import CurveItem
+import numpy as np
+from scipy import interpolate
 
 import ecg_plot
+
+
+def interp_series(s, start, stop, npoints):
+    f = interpolate.interp1d(
+        s.index,
+        s.values,
+        assume_sorted=True,
+        copy=False,
+        fill_value="extrapolate",
+    )
+    newidx = np.linspace(start, stop, num=npoints)
+    return f(newidx)
 
 
 def export_curves(
@@ -12,14 +24,16 @@ def export_curves(
     filepath: str,
     index_label: str = "timens",
 ) -> None:
-    sers = [c.series for c in curves]
+    beginns = min(c.series.index[0] for c in curves)
+    endns = max(c.series.index[-1] for c in curves)
+    duration_s = (endns - beginns) * 1e-9
+    samplerate = int(max([c.samplerate for c in curves]))
+    npoints = int(duration_s * samplerate)
+    resampled = [interp_series(c.series, beginns, endns, npoints) for c in curves]
+    ecg = np.vstack(resampled)
     names = [c.name() for c in curves]
-    data = pd.concat(sers, axis=1).sort_index()
-    # TODO: resample
-    ecg = data.to_numpy()
-    ecg = ecg.T
     ecg_plot.plot(
-        ecg, sample_rate=500, title=filepath.stem, lead_index=names, columns=1
+        ecg, sample_rate=samplerate, title=filepath.stem, lead_index=names, columns=1
     )
     ecg_plot.save_as_png(filepath.stem, f"{filepath.parent}/")
 
