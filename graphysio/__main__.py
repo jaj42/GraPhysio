94e6fce7d7214824cb930fec15b8cc9a8f87d1dd
Jona JOACHIM <jona@joachim.cc>
2025-09-10 10:41:59 +0200

    fix typo

--- graphysio/__main__.py
+++ graphysio/__main__.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    if args.shortcuts and shorcuts_avail:
+    if args.shortcuts and shortcuts_avail:
         mk_shortscuts()
     else:
         graphysio_main()
